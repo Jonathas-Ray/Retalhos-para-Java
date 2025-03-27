89a9b9070b225171d69670236b51ca1f053e8376
Jonathas Ray <jonathasrmcguimaraes@gmail.com>
2025-03-27 04:43:37 -0300

    Update InputMethods.java
    Aqui eu faço um polimorfismo estático chamado de polimorfismo overloading  nas funções para que eu possa chamar essas funções sem passar texto como parâmetro também.

--- InputMethods.java
+++ InputMethods.java
@@ -25,9 +25,47 @@ public class InputMethods {
         }
         return value;
     }
+    
+    public static int input_Int() {
+        int value = 0;
+        while (true) {
+            if (!(scanner.hasNextInt())) {
+                scanner.nextLine(); //Apaga o conteúdo do Buffer
+                System.out.println("O tipo do valor não está correto, insira novamente\n");
+            } else {
+                value = scanner.nextInt();
+                scanner.nextLine(); //Apaga a quebra de linha do Buffer
+                if (valueChecker(value) == 1) {
+                    break;
+                } else {
+                    System.out.println("Nesse caso, digite novamente");
+                }
+            }
+        }
+        return value;
+    }
 
     public static double input_Double(String output) {
         System.out.println(output);
+        Double value;
+        while (true) {
+            if (!(scanner.hasNextDouble())) {
+                scanner.nextLine(); //Apaga o conteúdo do Buffer
+                System.out.println("O tipo do valor não está correto, insira novamente\n");
+            } else {
+                value = scanner.nextDouble();
+                scanner.nextLine(); //Apaga a quebra de linha do Buffer
+                if (valueChecker(value) == 1) {
+                    break;
+                } else {
+                    System.out.println("Nesse caso, digite novamente");
+                }
+            }
+        }
+        return value;
+    }
+    
+    public static double input_Double() {
         Double value;
         while (true) {
             if (!(scanner.hasNextDouble())) {
@@ -60,9 +98,24 @@ public class InputMethods {
                     System.out.println("Nesse caso, digite novamente");
                 }
             }
-
         } while (true);
-
+        return value;
+    }
+    
+    public static String input_String() {
+        String value;
+        do {
+            value = scanner.nextLine();
+            if (value.equals("\n")) {
+                System.out.println("Valor inválido");
+            } else {
+                if (valueChecker(value) == 1) {
+                    break;
+                } else {
+                    System.out.println("Nesse caso, digite novamente");
+                }
+            }
+        } while (true);
         return value;
     }
 
